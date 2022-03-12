fbf09e87a285735824ea1413c09e009b61ffc43d
ThomasBV67 <thomasbureauviens@gmail.com>
2022-03-12 09:51:03 -0500

    Ajout d'une fonction de mergeSort Changement de l'emplacement des répertoires d'auteurs

--- src/main.py
+++ src/main.py
@@ -7,6 +7,7 @@
 from markov_CIP1_CIP2 import *
 
 test1 = markov()
+test1.set_aut_dir('resources')
 test1.set_auteurs()
 test1.set_ponc(False)
 test1.analyze()
--- src/markov_CIP1_CIP2.py
+++ src/markov_CIP1_CIP2.py
@@ -223,7 +223,7 @@ class markov():
         #   les mots d'une très longue oeuvre du même auteur. Ce n'est PAS ce qui vous est demandé ici.
         for authors in self.auteurs:
             for oeuvrePath in self.get_aut_files(authors):
-                currentText = open(oeuvrePath, 'r', encoding='latin1').read()
+                currentText = open(oeuvrePath, 'r', encoding='latin-1').read()
                 oeuvreData = {}
                 currentText = currentText.lower()
 
@@ -264,5 +264,46 @@ class markov():
                         self.DATA[authors]["normal"][word] = 1
             for word in self.DATA[authors]["normal"]:
                 self.DATA[authors]["normal"][word] /= totalWords
-
         return
+
+    def mergeSort(self, arr):
+        if len(arr) > 1:
+
+            # Finding the mid of the array
+            mid = len(arr) // 2
+
+            # Dividing the array elements
+            L = arr[:mid]
+
+            # into 2 halves
+            R = arr[mid:]
+
+            # Sorting the first half
+            mergeSort(L)
+
+            # Sorting the second half
+            mergeSort(R)
+
+            i = j = k = 0
+
+            # Copy data to temp arrays L[] and R[]
+            while i < len(L) and j < len(R):
+                if L[i] < R[j]:
+                    arr[k] = L[i]
+                    i += 1
+                else:
+                    arr[k] = R[j]
+                    j += 1
+                k += 1
+
+            # Checking if any element was left
+            while i < len(L):
+                arr[k] = L[i]
+                i += 1
+                k += 1
+
+            while j < len(R):
+                arr[k] = R[j]
+                j += 1
+                k += 1
+
